a375fb67fad5bb6a39141c1a76c75411508fdb42
cdesch <cdesch@gmail.com>
2020-03-11 15:30:31 -0400

    Testing more with ODBC

--- FireBirdConsoleExample/Program.cs
+++ FireBirdConsoleExample/Program.cs
@@ -4,6 +4,7 @@ using FirebirdSql.Data.FirebirdClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Logging;
+using System.Data.Odbc;
 
 namespace FireBirdConsoleExample
 {
@@ -12,6 +13,9 @@ namespace FireBirdConsoleExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //odbc2();
+            
+            //Odbc();
             string connectionString = "User ID=sysdba;Password=masterkey;Database=localhost:C:\\data\\demo.fdb;DataSource=localhost;Charset=NONE;";
             FirebirdEventListener(connectionString);
 
@@ -31,7 +35,160 @@ namespace FireBirdConsoleExample
             }
         }
 
+        static void odbc2()
+        {
+//            ODBC_EVENT_INFO eventInfo[] =
+//            {
+//                INIT_ODBC_EVENT("new_order"),
+//                INIT_ODBC_EVENT("change_order")
+//            };
+            //https://www.easysoft.com/developer/languages/csharp/ado-net-odbc.html
+            OdbcConnection DbConnection = new OdbcConnection("DSN=FireBirdDemoDB");
+            DbConnection.InfoMessage +=  (sender, e) => Console.WriteLine($"Event: {e}");
 
+            DbConnection.Open();
+            OdbcCommand DbCommand = DbConnection.CreateCommand();
+            DbCommand.CommandText = "SELECT * FROM DEMO";
+            OdbcDataReader DbReader = DbCommand.ExecuteReader();
+
+            int fCount = DbReader.FieldCount;
+            Console.Write(":");
+            for ( int i = 0; i < fCount; i ++ )
+            {
+                String fName = DbReader.GetName(i);
+                Console.Write( fName + ":");
+            }
+            Console.WriteLine();
+
+            while( DbReader.Read()) 
+            {
+                Console.Write( ":" );
+                for (int i = 0; i < fCount; i++)
+                {
+                    String col = DbReader.GetString(i);
+                        
+                    Console.Write(col + ":");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            DbReader.Close();
+            DbCommand.Dispose();
+            DbConnection.Close();
+        }
+
+        static void Odbc()
+        {
+            //https://dev.mysql.com/doc/connector-odbc/en/connector-odbc-examples-programming-net-csharp.html
+            try
+        {
+          //Connection string for Connector/ODBC 3.51
+          string MyConString = "DRIVER={FireBird DemoDB};" +
+            "SERVER=localhost;" +
+            "DATABASE=demo;" +
+            "UID=SYSDBA;" +
+            "PASSWORD=masterkey;";
+
+          //Connect to MySQL using Connector/ODBC
+          OdbcConnection MyConnection = new OdbcConnection(MyConString);
+          MyConnection.Open();
+
+          Console.WriteLine("\n !!! success, connected successfully !!!\n");
+
+          //Display connection information
+          Console.WriteLine("Connection Information:");
+          Console.WriteLine("\tConnection String:" +
+                            MyConnection.ConnectionString);
+          Console.WriteLine("\tConnection Timeout:" +
+                            MyConnection.ConnectionTimeout);
+          Console.WriteLine("\tDatabase:" +
+                            MyConnection.Database);
+          Console.WriteLine("\tDataSource:" +
+                            MyConnection.DataSource);
+          Console.WriteLine("\tDriver:" +
+                            MyConnection.Driver);
+          Console.WriteLine("\tServerVersion:" +
+                            MyConnection.ServerVersion);
+
+          //Create a sample table
+          OdbcCommand MyCommand =
+            new OdbcCommand("DROP TABLE IF EXISTS my_odbc_net",
+                            MyConnection);
+          MyCommand.ExecuteNonQuery();
+          MyCommand.CommandText =
+            "CREATE TABLE my_odbc_net(id int, name varchar(20), idb bigint)";
+          MyCommand.ExecuteNonQuery();
+
+          //Insert
+          MyCommand.CommandText =
+            "INSERT INTO my_odbc_net VALUES(10,'venu', 300)";
+          Console.WriteLine("INSERT, Total rows affected:" +
+                            MyCommand.ExecuteNonQuery());;
+
+          //Insert
+          MyCommand.CommandText =
+            "INSERT INTO my_odbc_net VALUES(20,'mysql',400)";
+          Console.WriteLine("INSERT, Total rows affected:" +
+                            MyCommand.ExecuteNonQuery());
+
+          //Insert
+          MyCommand.CommandText =
+            "INSERT INTO my_odbc_net VALUES(20,'mysql',500)";
+          Console.WriteLine("INSERT, Total rows affected:" +
+                            MyCommand.ExecuteNonQuery());
+
+          //Update
+          MyCommand.CommandText =
+            "UPDATE my_odbc_net SET id=999 WHERE id=20";
+          Console.WriteLine("Update, Total rows affected:" +
+                            MyCommand.ExecuteNonQuery());
+
+          //COUNT(*)
+          MyCommand.CommandText =
+            "SELECT COUNT(*) as TRows FROM my_odbc_net";
+          Console.WriteLine("Total Rows:" +
+                            MyCommand.ExecuteScalar());
+
+          //Fetch
+          MyCommand.CommandText = "SELECT * FROM my_odbc_net";
+          OdbcDataReader MyDataReader;
+          MyDataReader =  MyCommand.ExecuteReader();
+          while (MyDataReader.Read())
+            {
+              if(string.Compare(MyConnection.Driver,"myodbc3.dll") == 0) {
+                //Supported only by Connector/ODBC 3.51
+                Console.WriteLine("Data:" + MyDataReader.GetInt32(0) + " " +
+                                  MyDataReader.GetString(1) + " " +
+                                  MyDataReader.GetInt64(2));
+              }
+              else {
+                //BIGINTs not supported by Connector/ODBC
+                Console.WriteLine("Data:" + MyDataReader.GetInt32(0) + " " +
+                                  MyDataReader.GetString(1) + " " +
+                                  MyDataReader.GetInt32(2));
+              }
+            }
+
+          //Close all resources
+          MyDataReader.Close();
+          MyConnection.Close();
+        }
+      catch (OdbcException MyOdbcException) //Catch any ODBC exception ..
+        {
+          for (int i=0; i < MyOdbcException.Errors.Count; i++)
+            {
+              Console.Write("ERROR #" + i + "\n" +
+                            "Message: " +
+                            MyOdbcException.Errors[i].Message + "\n" +
+                            "Native: " +
+                            MyOdbcException.Errors[i].NativeError.ToString() + "\n" +
+                            "Source: " +
+                            MyOdbcException.Errors[i].Source + "\n" +
+                            "SQL: " +
+                            MyOdbcException.Errors[i].SQLState + "\n");
+            }
+        }
+        }
 
         static void FirebirdEventListener(string connectionString)
         {
@@ -39,7 +196,7 @@ namespace FireBirdConsoleExample
             {
                 events.RemoteEventCounts += (sender, e) => Console.WriteLine($"Event: {e.Name} | Counts: {e.Counts}");
                 events.RemoteEventError += (sender, e) => Console.WriteLine($"ERROR: {e.Error}");
-                //events.QueueEvents("EVENT1", "EVENT2", "EVENT3", "EVENT4");
+                events.QueueEvents("NEWFOOBARD_POSTEVENT", "OtherEvent");
                 Console.WriteLine("Listening...");
                 Console.ReadLine();
             }
